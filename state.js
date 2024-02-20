6eb6d0c542f282243635d792693ccd1f140da522
simonemoretti94 <simone.moretti.944@gmail.com>
2024-02-20 17:52:55 +0100

    1.6th fixed a dumb save error, look at 1.5th commit in order to go on ahead

--- state.js
+++ state.js
@@ -5,7 +5,9 @@ export const state = reactive({
     apiKey: '4529728a455fd554eacb1d7df41d13e5',
     cards: [],
 
-    FoundLoading: true,
+    FoundLoading: true, //return cards array length
+    loadingAPI: true, //while true, show a loading icon
+
 });
 
 //https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro
