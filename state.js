cfa3029053b6473a49825eba34c929e9a9e5fa02
simonemoretti94 <simone.moretti.944@gmail.com>
2024-02-22 13:52:46 +0100

    1.7th added infos

--- state.js
+++ state.js
@@ -11,3 +11,17 @@ export const state = reactive({
 });
 
 //https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro
+
+//https://image.tmdb.org/t/p/w342/vKp3NvqBkcjHkCHSGi6EbcP7g4J.jpg
+
+//https://image.tmdb.org/t/p/w342/ + backdrop_path
+
+// poster_sizes": [
+//     "w92",
+//     "w154",
+//     "w185",
+//     "w342",
+//     "w500",
+//     "w780",
+//     "original"
+//   ],
