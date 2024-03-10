990cc13cea5ceded3355d74dfeac613f5886b008
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-10 13:54:20 +0100

    8.1th added hover effect on header's new icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -84,7 +84,7 @@ export default {
             <h2>boolflix</h2>
             <input id="header_input" class="d-none" type="text" placeholder="Write here a movie and press enter" v-model="state.selectedMovie"
                 @keyup="movieSearch($event.target.value)">
-            <i id="show_icon" class="fa-regular fa-pen-to-square fa-flip-horizontal fa-lg" style="color: #ffffff;" ref="show_icon" @click="iconClick($event)" ></i>
+            <i id="show_icon" class="fa-regular fa-pen-to-square fa-flip-horizontal fa-lg" ref="show_icon" @click="iconClick($event)" ></i>
         </div>
     </header>
 </template>
@@ -125,6 +125,18 @@ header {
             color: rgb(53, 53, 53);
             font-size: small;
             /* display: none; */
+        }
+
+        & i#show_icon {
+            color: #ffffff;
+            text-shadow: 1.5px -1.5px black;
+            filter: drop-shadow( 0 0 1.5px rgba(255, 255, 255, 0.659));
+        }
+
+        & i#show_icon:hover {
+            color: #ffffff;
+            text-shadow: 1.5px -1.5px black;
+            filter: drop-shadow( 0 0 1.5px #ff0000c4);
         }
     }
 }
